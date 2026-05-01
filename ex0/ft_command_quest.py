def2e15d5e6c062c7aa104e4e5eec13352dae3a8
Bruno Daniel Ramalho Cruz Gomes <bramalho@c2r1s7.42porto.com>
2026-05-01 21:32:48 +0100

    concluded ex0 and 1 , started ex2

--- ex0/ft_command_quest.py
+++ ex0/ft_command_quest.py
@@ -2,24 +2,24 @@
 
 import sys
 
+
 def ft_command_quest() -> None:
     arg_count = len(sys.argv)
     args_only = sys.argv[1:]
     name = sys.argv[0]
 
+    print("=== Command Quest ===")
+    print(f"Program name: {name}")
+
     if arg_count == 1:
-        print("=== Command Quest ===")
-        print(f"Program name: {name}")
         print("No arguments provided!")
-        print(f"Total arguments {arg_count}")
+        print(f"Total arguments: {arg_count}")
     else:
-        print("=== Command Quest ===")
-        print(f"Program name: {name}")
-        print(f"Arguments received: {args_only}")
-        for i in args_only:
+        print(f"Arguments received: {len(args_only)}")
+        for i in range(1, arg_count):
             print(f"Argument {i}: {sys.argv[i]}")
         print(f"Total Arguments: {arg_count}")
-        
+
+
 if __name__ == "__main__":
     ft_command_quest()
-
---
+++ ex1/ft_score_analytics.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+
+
+def ft_score_analytics() -> None:
+
+    if len(sys.argv) < 2:
+        print("No scores provided. "
+              "Usage: python3 ft_score_analytics.py <score1> <score2>")
+        return
+
+    arguments = sys.argv[1:]
+
+    valid_scores = []
+    for arg in arguments:
+        try:
+            score = int(arg)
+            valid_scores.append(score)
+        except ValueError as e:
+            print(f"Invalid parameter: {e}")
+            continue
+
+    total_players = len(arguments)
+    total_score = sum(valid_scores)
+    average_score = sum(valid_scores) / total_players
+    highest_score = max(valid_scores)
+    lowest_score = min(valid_scores)
+    score_range = max(valid_scores) - min(valid_scores)
+
+    print(f"Total Players: {total_players}")
+    print(f"Total Score: {total_score}")
+    print(f"Average Score: {average_score}")
+    print(f"Highest Score: {highest_score}")
+    print(f"Lowest Score: {lowest_score}")
+    print(f"Score Range: {score_range}")
+
+
+if __name__ == "__main__":
+    ft_score_analytics()
