2c74224c443f7d2982699a6c53955c7528d7ae8e
Bruno Daniel Ramalho Cruz Gomes <bramalho@c2r1s10.42porto.com>
2026-05-06 19:29:21 +0100

    started main list and first 3 prints on the exercise

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+
+PLAYERS: list[str] = ["Alice", "bob", "Charlie", "dylan", "Emma",
+                      "Gregory", "john", "kevin", "Liam"]
+
+
+#list comprehensions 
+
+def main() -> None:
+    print(f"Initial list of players: {PLAYERS}")
+    print()
+    all_cap: list[str] = [name.capitalize() for name in PLAYERS]
+    print(f"New list with all names capitalized: {all_cap}")
+    print()
+    only_cap: list[str] = [name for name in PLAYERS if name[0].isupper()]
+    print(f"New list of capitalized names only: {only_cap}")
+
+if __name__ == "__main__":
+    main()
