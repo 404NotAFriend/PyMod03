22375fba6e604f3de87d382266b22ee9db145568
Bruno Daniel Ramalho Cruz Gomes <bramalho@c2r1s10.42porto.com>
2026-05-06 14:57:09 +0100

    created base gen_even , player and actions lists and main() for testing

--- ex5/ft_data_stream.py
+++ ex5/ft_data_stream.py
@@ -1,2 +1,25 @@
 #!/usr/bin/env python3
 import typing
+import random
+
+
+PLAYERS: list[str] = ["Alice", "Bob", "Charlie", "Dylan"]
+ACTIONS: list[str] = ["run", "eat", "sleep", "grab"]
+
+
+def gen_event() -> typing.Generator[tuple[str, str], None, None]:
+    while True:
+        name: str = random.choice(PLAYERS)
+        action: str = random.choice(ACTIONS)
+        yield (name, action)
+
+
+def main() -> None:
+    generator = gen_event()
+    print(next(generator))
+    print(next(generator))
+    print(next(generator))
+
+
+if __name__ == "__main__":
+    main()
