134acbaf10f79a99a6f777c2a304386898f33cb3
404NotAFriend <brunodrcgomes@gmail.com>
2026-05-06 05:38:25 +0100

    started to solve ex4

--- ex4/ft_inventory_system.py
+++ ex4/ft_inventory_system.py
@@ -1,3 +1,28 @@
 #!/usr/bin/env python3
+import sys
+
+
+def parse_inventory() -> dict[str, int]:
+
+    arg_count: int = len(sys.argv)
+    arguments: list[str] = sys.argv[1:]
+
+    if arg_count < 2:
+        return
+
+    inventory: dict[str, int] = {}
+
+    for arg in arguments:
+        check_parts: list[str] = arg.split(":")
+        if len(check_parts) != 2:
+            print(f"Error - invalid parameter '{arg}'")
+        else:
+            try:
+                item_name: str = check_parts[0].strip()
+                quantity: int = int(check_parts[1])
+                inventory.item_name = check_parts[0]
+                inventory.quantity = check_parts[1]
+            except ValueError as e:
+                print(f"Quantity error for '{item_name}': {e}")
 
 
