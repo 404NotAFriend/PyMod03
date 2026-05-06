0aafb30898dcc373462183049e53a3c237989cac
Bruno Daniel Ramalho Cruz Gomes <bramalho@c2r1s10.42porto.com>
2026-05-06 20:33:07 +0100

    created the random dict for scores

--- ex6/ft_data_alchemist.py
+++ ex6/ft_data_alchemist.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
+import random
 
 PLAYERS: list[str] = ["Alice", "bob", "Charlie", "dylan", "Emma",
                       "Gregory", "john", "kevin", "Liam"]
 
 
-#list comprehensions 
-
 def main() -> None:
-    print(f"Initial list of players: {PLAYERS}")
+    print("=== Game Data Alchemist ===")
     print()
+    print(f"Initial list of players: {PLAYERS}")
     all_cap: list[str] = [name.capitalize() for name in PLAYERS]
     print(f"New list with all names capitalized: {all_cap}")
-    print()
     only_cap: list[str] = [name for name in PLAYERS if name[0].isupper()]
     print(f"New list of capitalized names only: {only_cap}")
+    print()
+
+    score: dict[str, int] = {
+        name: random.randint(0, 1000) for name in all_cap
+    }
+    print(f"Score dict: {score}")
+
 
 if __name__ == "__main__":
     main()
